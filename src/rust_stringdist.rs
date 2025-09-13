0886badb0f7a39db68c4333e61b16cd458653bb4
Niklas von Moers <niklas.von.moers2@t-online.de>
2025-09-13 20:42:34 +0200

    improve conversion

--- src/rust_stringdist.rs
+++ src/rust_stringdist.rs
@@ -15,46 +15,17 @@ impl<'py> IntoPyObject<'py> for EditOperation {
 
     /// Converts the `EditOperation` into a Python tuple
     fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
-        let elements: Vec<PyObject> = match self {
+        match self {
             EditOperation::Substitute {
                 source,
                 target,
                 cost,
-            } => {
-                vec![
-                    "substitute".into_pyobject(py)?.into(),
-                    source.into_pyobject(py)?.into(),
-                    target.into_pyobject(py)?.into(),
-                    cost.into_pyobject(py)?.into(),
-                ]
-            }
-            EditOperation::Insert { target, cost } => {
-                vec![
-                    "insert".into_pyobject(py)?.into(),
-                    py.None(),
-                    target.into_pyobject(py)?.into(),
-                    cost.into_pyobject(py)?.into(),
-                ]
-            }
-            EditOperation::Delete { source, cost } => {
-                vec![
-                    "delete".into_pyobject(py)?.into(),
-                    source.into_pyobject(py)?.into(),
-                    py.None(),
-                    cost.into_pyobject(py)?.into(),
-                ]
-            }
-            EditOperation::Match { token } => {
-                vec![
-                    "match".into_pyobject(py)?.into(),
-                    token.clone().into_pyobject(py)?.into(),
-                    token.into_pyobject(py)?.into(),
-                    0.0f64.into_pyobject(py)?.into(),
-                ]
-            }
-        };
-
-        PyTuple::new(py, elements)
+            } => ("substitute", Some(source), Some(target), cost),
+            EditOperation::Insert { target, cost } => ("insert", None, Some(target), cost),
+            EditOperation::Delete { source, cost } => ("delete", Some(source), None, cost),
+            EditOperation::Match { token } => ("match", Some(token.clone()), Some(token), 0.0),
+        }
+        .into_pyobject(py)
     }
 }
 
